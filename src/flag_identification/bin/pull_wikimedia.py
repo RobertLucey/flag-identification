6c7a6cddd4d2ee165e132823a237fa2217bac987
Robert Lucey <robert@glimpse.ie>
2023-01-11 20:11:21 +0000

    Improve skipping
    Also download all flags, can filter after the fact if we want

--- src/flag_identification/bin/pull_wikimedia.py
+++ src/flag_identification/bin/pull_wikimedia.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+from urllib.parse import unquote
 from multiprocessing.pool import ThreadPool
 
 import bs4
@@ -7,7 +9,6 @@ from PIL import Image
 import aspose.words as aw
 
 from flag_identification.settings import RAW_DIR
-from flag_identification.utils import is_text_flaggy
 
 
 HEADERS = {
@@ -26,132 +27,187 @@ def process(url):
     imgs = body.find_all("img")
 
     for img in imgs:
+
+        src = os.path.basename(img.get('src'))
+        src = src.replace('_', ' ')
+
+        while Path(src).suffix in {'.png', '.gif', '.jpg', '.jpeg', '.svg', '.PNG', '.webp', '.SVG', '.JPG', '.tif'}:
+            src = str(Path(src).with_suffix(''))
+        src = src + '.png'
+        src = unquote(src)
+        src_path = os.path.join(RAW_DIR, category_modified + '_' + src)
+
+        if os.path.exists(src_path):
+            print(f'Already exists: {src_path}')
+            continue
+
+        if '-' in src:
+            src_path = os.path.join(RAW_DIR, category_modified + '_' + '-'.join(src.split('-')[1:]))
+            if os.path.exists(src_path):
+                print(f'Already exists: {src_path}')
+                continue
+
+        print(f'GET: {img.get("src")}       {img.parent.get("href")}')
+
         if img.parent.get("href"):
-            if is_text_flaggy(img.parent.get("href")):
-                url = "https://commons.wikimedia.org" + img.parent.get("href")
+            url = "https://commons.wikimedia.org" + img.parent.get("href")
 
-                if url.endswith("svg"):
-                    response = requests.get(url)
-                    soup = bs4.BeautifulSoup(response.text)
-                    link = soup.find("div", {"id": "file", "class": "fullImageLink"})
-                    imgs = link.find_all("img")
+            if url.endswith("svg"):
+                response = requests.get(url)
+                soup = bs4.BeautifulSoup(response.text)
+                link = soup.find("div", {"id": "file", "class": "fullImageLink"})
+                imgs = link.find_all("img")
 
-                    if len(imgs) > 1:
-                        print("too many images")
+                if len(imgs) > 1:
+                    print("too many images")
 
-                    name = imgs[0].get("alt").replace("File:", "")
-                    url = imgs[0].get("src")
+                name = imgs[0].get("alt").replace("File:", "")
+                url = imgs[0].get("src")
 
-                    tmp_path = f"/tmp/{name}"
-                    file_path = os.path.join(
-                        RAW_DIR,
-                        f'{category_modified}_{name.replace(".svg", ".png").replace(".SVG", ".png")}',
-                    )
+                tmp_path = f"/tmp/{name}"
+                file_path = os.path.join(
+                    RAW_DIR,
+                    f'{category_modified}_{name.replace(".svg", ".png").replace(".SVG", ".png")}',
+                )
 
-                    if os.path.exists(file_path):
-                        continue
+                if os.path.exists(file_path):
+                    print(f'Already exists: {file_path}')
+                    continue
 
-                    img_data = requests.get(url, headers=HEADERS).content
-                    with open(tmp_path, "wb") as handler:
+                img_data = requests.get(url, headers=HEADERS).content
+                with open(tmp_path, "wb") as handler:
+                    handler.write(img_data)
+
+                doc = aw.Document()
+                builder = aw.DocumentBuilder(doc)
+
+                try:
+                    shape = builder.insert_image(f"/tmp/{name}")
+                except:
+                    print(f"Bad svg: {url}")
+                    continue
+
+                page_setup = builder.page_setup
+                page_setup.page_width = shape.width
+                page_setup.page_height = shape.height
+                page_setup.top_margin = 0
+                page_setup.left_margin = 0
+                page_setup.bottom_margin = 0
+                page_setup.right_margin = 0
+
+                # save as PNG
+                try:
+                    doc.save(file_path)
+                except:
+                    print(f"Failed to save svg as png: {url}")
+
+            elif url.endswith("jpg") or url.endswith("JPG"):
+
+                response = requests.get(url)
+                soup = bs4.BeautifulSoup(response.text)
+                link = soup.find("div", {"id": "file", "class": "fullImageLink"})
+                imgs = link.find_all("img")
+
+                if len(imgs) > 1:
+                    print(f"too many images: {url}")
+
+                name = imgs[0].get("alt").replace("File:", "")
+                url = imgs[0].get("src")
+
+                tmp_path = f"/tmp/{name}"
+                file_path = os.path.join(
+                    RAW_DIR,
+                    f'{category_modified}_{name.replace(".JPG", ".png").replace(".jpg", ".png")}',
+                )
+
+                if os.path.exists(file_path):
+                    print(f'Already exists: {file_path}')
+                    continue
+
+                img_data = requests.get(url, headers=HEADERS).content
+                with open(tmp_path, "wb") as handler:
+                    handler.write(img_data)
+
+                try:
+                    im = Image.open(f"/tmp/{name}")
+                except:
+                    print(f"Could not open image: /tmp/{name}    {url}")
+
+                try:
+                    im.save(file_path)
+                except:
+                    print(f"Cannot save as png: /tmp/{name}    {url}")
+
+            elif url.endswith("png") or url.endswith("PNG"):
+
+                response = requests.get(url)
+                soup = bs4.BeautifulSoup(response.text)
+                link = soup.find("div", {"id": "file", "class": "fullImageLink"})
+                imgs = link.find_all("img")
+
+                if len(imgs) > 1:
+                    print("too many images")
+
+                name = imgs[0].get("alt").replace("File:", "")
+                url = imgs[0].get("src")
+
+                file_path = os.path.join(
+                    RAW_DIR, category_modified + "_" + name.replace(".PNG", ".png")
+                )
+
+                if os.path.exists(file_path):
+                    print(f'Already exists: {file_path}')
+                    continue
+
+                img_data = requests.get(url, headers=HEADERS).content
+                try:
+                    with open(file_path, "wb") as handler:
                         handler.write(img_data)
+                except:
+                    print(f"Can't write to file: {file_path}    {url}")
 
-                    doc = aw.Document()
-                    builder = aw.DocumentBuilder(doc)
+            elif url.endswith("gif") or url.endswith("gif"):
 
-                    try:
-                        shape = builder.insert_image(f"/tmp/{name}")
-                    except:
-                        print(f"Bad svg: {url}")
-                        continue
+                response = requests.get(url)
+                soup = bs4.BeautifulSoup(response.text)
+                link = soup.find("div", {"id": "file", "class": "fullImageLink"})
+                imgs = link.find_all("img")
 
-                    page_setup = builder.page_setup
-                    page_setup.page_width = shape.width
-                    page_setup.page_height = shape.height
-                    page_setup.top_margin = 0
-                    page_setup.left_margin = 0
-                    page_setup.bottom_margin = 0
-                    page_setup.right_margin = 0
+                if len(imgs) > 1:
+                    print(f"too many images: {url}")
 
-                    # save as PNG
-                    try:
-                        doc.save(file_path)
-                    except:
-                        print(f"Failed to save svg as png: {url}")
+                name = imgs[0].get("alt").replace("File:", "")
+                url = imgs[0].get("src")
 
-                elif url.endswith("jpg") or url.endswith("JPG"):
-                    response = requests.get(url)
-                    soup = bs4.BeautifulSoup(response.text)
-                    link = soup.find("div", {"id": "file", "class": "fullImageLink"})
-                    imgs = link.find_all("img")
+                tmp_path = f"/tmp/{name}"
+                file_path = os.path.join(
+                    RAW_DIR,
+                    f'{category_modified}_{name.replace(".gif", ".png").replace(".gif", ".png")}',
+                )
 
-                    if len(imgs) > 1:
-                        print(f"too many images: {url}")
+                if os.path.exists(file_path):
+                    print(f'Already exists: {file_path}')
+                    continue
 
-                    name = imgs[0].get("alt").replace("File:", "")
-                    url = imgs[0].get("src")
+                img_data = requests.get(url, headers=HEADERS).content
+                with open(tmp_path, "wb") as handler:
+                    handler.write(img_data)
 
-                    tmp_path = f"/tmp/{name}"
-                    file_path = os.path.join(
-                        RAW_DIR,
-                        f'{category_modified}_{name.replace(".JPG", ".png").replace(".jpg", ".png")}',
-                    )
+                try:
+                    im = Image.open(f"/tmp/{name}")
+                except:
+                    print(f"Could not open image: /tmp/{name}    {url}")
 
-                    if os.path.exists(file_path):
-                        continue
+                try:
+                    im.save(file_path, 'png')
+                except:
+                    print(f"Cannot save as png: /tmp/{name}    {url}")
 
-                    img_data = requests.get(url, headers=HEADERS).content
-                    with open(tmp_path, "wb") as handler:
-                        handler.write(img_data)
-
-                    try:
-                        im = Image.open(f"/tmp/{name}")
-                    except:
-                        print(f"Could not open image: /tmp/{name}    {url}")
-
-                    try:
-                        im.save(file_path)
-                    except:
-                        print(f"Cannot save as png: /tmp/{name}    {url}")
-
-                elif url.endswith("png") or url.endswith("PNG"):
-
-                    response = requests.get(url)
-                    soup = bs4.BeautifulSoup(response.text)
-                    link = soup.find("div", {"id": "file", "class": "fullImageLink"})
-                    imgs = link.find_all("img")
-
-                    if len(imgs) > 1:
-                        print("too many images")
-
-                    name = imgs[0].get("alt").replace("File:", "")
-                    url = imgs[0].get("src")
-
-                    file_path = os.path.join(
-                        RAW_DIR, category_modified + "_" + name.replace(".PNG", ".png")
-                    )
-
-                    if os.path.exists(file_path):
-                        continue
-
-                    img_data = requests.get(url, headers=HEADERS).content
-                    try:
-                        with open(file_path, "wb") as handler:
-                            handler.write(img_data)
-                    except:
-                        print(f"Can't write to file: {file_path}    {url}")
-
-                elif url.endswith("gif") or url.endswith("gif"):
-                    print("gif")
-
-                elif url.endswith("jpeg") or url.endswith("JPEG"):
-                    print("jpeg")
-
-                else:
-                    print(f"not sure what to do with {url}")
+            elif url.endswith("jpeg") or url.endswith("JPEG"):
+                print(f'jpeg {img.parent.get("href")}')
 
             else:
-                # FIXME: Can worry about these later
-                print(f'NOT FLAGGY ENOUGH NAME: {img.parent.get("href")}')
+                print(f"not sure what to do with {url}")
 
 
 def main():
