c88dadd523816ce542cdb53a31fbaa36bc470eb9
Robert Lucey <robert@glimpse.ie>
2023-03-04 15:46:47 +0000

    Bump 0.0.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name="flag_identification",
-    version="0.0.5",
+    version="0.0.6",
     python_requires=">=3.6",
     description="Identify a flag by an image",
     long_description="Identify a flag by an image",
