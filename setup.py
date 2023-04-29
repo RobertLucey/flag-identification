c9b0057a92090dea940cb41c3815a9e5b6388fa2
Robert Lucey <robert.lucey@visionr.com>
2023-04-29 11:25:26 +0100

    Bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name="flag_identification",
-    version="0.0.6",
+    version="0.0.7",
     python_requires=">=3.6",
     description="Identify a flag by an image",
     long_description="Identify a flag by an image",
